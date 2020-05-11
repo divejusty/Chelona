e289652ccfa8f523570b99e12ff90e898762e7ef
Justin Wies <justin@wies.nl>
2020-05-11 14:05:37 +0200

    Restructured for use with autoloading

--- src/Chelona/Request.php
+++ src/Chelona/Request.php
@@ -4,6 +4,11 @@ namespace Chelona;
 
 class Request
 {
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
 	public static function uri()
 	{
 		$uri = $_SERVER['REQUEST_URI'];
@@ -11,11 +16,21 @@ class Request
 		return parse_url($uri, PHP_URL_PATH);
 	}
 
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
 	public static function method()
 	{
 		return $_SERVER['REQUEST_METHOD'];
 	}
 
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
 	public static function all()
 	{
 		return $_SERVER;

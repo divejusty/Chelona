9293ac5f55e917a789b19f1637c78a5f6e247497
Justin Wies <justin@goedemiddag.nl>
2024-10-04 16:22:06 +0200

    Update Container.php

--- src/Chelona/Shell/Data/Container.php
+++ src/Chelona/Shell/Data/Container.php
@@ -4,93 +4,93 @@ namespace Chelona\Shell\Data;
 
 class Container
 {
+    /****************************
+     * Private Fields + Methods *
+     ***************************/
 
-	/****************************
-	 * Private Fields + Methods *
-	 ***************************/
+    private array $data;
 
-	private $data = [];
+    private function __construct($data)
+    {
+        $this->data = $data;
+    }
 
-	private function __construct($data)
-	{
-		$this->data = $data;
-	}
+    /******************
+     * Static Methods *
+     *****************/
 
-	/******************
-	 * Static Methods *
-	 *****************/
+    // TODO: Look into creation of arrays with named keys and extra functionality with that
 
-	// TODO: Look into creation of arrays with named keys and extra functionality with that
+    /**
+     * Create a new Container instance with the specified data as it's contents.
+     *
+     * @param  mixed  ...$data
+     * @return Container
+     */
+    public static function create(... $data): Container
+    {
+        // Check if $data is off the form [[...]] due to a single array being passed into it
+        // If so, simplify the creation
+        if (count($data) === 1 && is_array($data[0])) {
+            return new Container($data[0]);
+        }
 
-	/**
-	 * Create a new Container instance with the specified data as it's contents.
-	 * 
-	 * @return Container 
-	 */
-	public static function create(... $data): Container
-	{
-		// Check if $data is off the form [[...]] due to a single array being passed into it
-		// If so, simplify the creation
-		if(is_array($data) && count($data) == 1 && is_array($data[0])) {
-			return new Container($data[0]);
-		}
+        return new Container($data);
+    }
 
-		return new Container($data);
-	}
+    /**
+     * Create a new, empty Container instance.
+     *
+     * @return Container
+     */
+    public static function empty(): Container
+    {
+        return new Container([]);
+    }
 
-	/**
-	 * Create a new, empty Container instance.
-	 * 
-	 * @return Container
-	 */
-	public static function empty(): Container
-	{
-		return new Container([]);
-	}
+    /**********************
+     * Non-Static Methods *
+     *********************/
 
-	/**********************
-	 * Non-Static Methods *
-	 *********************/
+    // Method ideas: Filter, exists, find, add/append, remove, sorting
 
-	// Method ideas: Filter, exists, find, add/append, remove, sorting
+    /**
+     * Maps the given callback over the data in the container, in place.
+     *
+     * @param callable $callback
+     *
+     * @return Container
+     */
+    public function map(callable $callback): Container
+    {
+        $this->data = array_map($callback, $this->data);
 
-	/**
-	 * Maps the given callback over the data in the container, in place.
-	 * 
-	 * @param callable $callback 
-	 * 
-	 * @return Container
-	 */
-	public function map(callable $callback): Container
-	{
-		$this->data = array_map($callback, $this->data);
+        return $this;
+    }
 
-		return $this;
-	}
-
-	/**
-	 * Creates a new Container with the same data as the current one
-	 * 
-	 * @return Container
-	 */
-	public function clone(): Container
-	{
-		return new Container($this->data);
-	}
+    /**
+     * Creates a new Container with the same data as the current one
+     *
+     * @return Container
+     */
+    public function clone(): Container
+    {
+        return new Container($this->data);
+    }
 
 
-	/**********
-	* Getters *
-	**********/
+    /**********
+    * Getters *
+    **********/
 
-	/**
-	 * Return the contents of the Container as an array.
-	 * 
-	 * @return array
-	 */
-	public function toArray(): array
-	{
-		return $this->data;
-	}
+    /**
+     * Return the contents of the Container as an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->data;
+    }
 
 }
